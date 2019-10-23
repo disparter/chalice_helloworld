26204cb2ebb5a9ec677b61d9aa046b69b6a21ea0
Felipe Faria <fefaria@uolinc.com>
2019-10-23 17:34:49 -0300

    Criado novo endpoint
    Criado endpoint para buscar o estado de uma cidade

--- app.py
+++ app.py
@@ -2,28 +2,17 @@ from chalice import Chalice
 
 app = Chalice(app_name='helloworld')
 
+CITIES_TO_STATE = {
+    'seattle': 'WA',
+    'portland': 'OR',
+}
+
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/cities/{city}')
+def state_of_city(city):
+    return {'state': CITIES_TO_STATE[city]}
