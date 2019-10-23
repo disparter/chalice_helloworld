8c16bba1b12609c65bb75f8033f24920cd1289f9
Felipe Faria <fefaria@uolinc.com>
2019-10-23 18:02:23 -0300

    Adicionado Log na aplicação
    Utilizando Eliot para realizar os logs

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from chalice import Chalice, BadRequestError
+from eliot import start_action
+from eliot.stdlib import EliotHandler
 
 app = Chalice(app_name='helloworld')
+app.log.addHandler(EliotHandler())
 app.debug = True
 
 CITIES_TO_STATE = {
@@ -16,8 +19,10 @@ def index():
 
 @app.route('/cities/{city}')
 def state_of_city(city):
-    try:
-        return {'state': CITIES_TO_STATE[city]}
-    except KeyError:
-        raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
-            city, ', '.join(CITIES_TO_STATE.keys())))
+    with start_action(action_type="helloworld:state_of_city", city=city):
+        try:
+            return {'state': CITIES_TO_STATE[city]}
+        except KeyError:
+            raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
+                city, ', '.join(CITIES_TO_STATE.keys())))
+
